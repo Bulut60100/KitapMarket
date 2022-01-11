1f469940c731d9b83d955300b8fd2b3dcaa195fc
Muhammed Mustafa Bulut <56533891+Bulut60100@users.noreply.github.com>
2022-01-11 20:55:51 +0300

    Update index.blade.php

--- BookShop/resources/views/front/index.blade.php
+++ BookShop/resources/views/front/index.blade.php
@@ -48,33 +48,21 @@ use App\Helper\mHelper;
 <div class="about">
     <div class="container">
         <div class="about-top grid-1">
+        
+            @foreach (\App\Models\kitaplar::inRandomOrder()->limit(3)->get() as $key => $value)
+
             <div class="col-md-4 about-left">
                 <figure class="effect-bubba">
-                    <img class="img-responsive" src="images/abt-1.jpg" alt=""/>
+                    <img class="img-responsive" src="{{asset($value['image'])}}" alt=""/>
                     <figcaption>
-                        <h2>Nulla maximus nunc</h2>
-                        <p>In sit amet sapien eros Integer dolore magna aliqua</p>
+                        <h2>{{$value['name']}}</h2>
+                        <p>{{$value['aciklama']}}</p>
                     </figcaption>
                 </figure>
             </div>
-            <div class="col-md-4 about-left">
-                <figure class="effect-bubba">
-                    <img class="img-responsive" src="images/abt-2.jpg" alt=""/>
-                    <figcaption>
-                        <h4>Mauris erat augue</h4>
-                        <p>In sit amet sapien eros Integer dolore magna aliqua</p>
-                    </figcaption>
-                </figure>
-            </div>
-            <div class="col-md-4 about-left">
-                <figure class="effect-bubba">
-                    <img class="img-responsive" src="images/abt-3.jpg" alt=""/>
-                    <figcaption>
-                        <h4>Cras elit mauris</h4>
-                        <p>In sit amet sapien eros Integer dolore magna aliqua</p>
-                    </figcaption>
-                </figure>
-            </div>
+
+            @endforeach
+
             <div class="clearfix"></div>
         </div>
     </div>
